347bbf2d73fb4ea69b8287c80cf82393acef9e75
Slavik <slavik1058@gmail.com>
2025-01-30 15:36:53 +0100

    change the result variable in task-3 file

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function getElementWidth(content, padding, border){
-    const result = Number.parseFloat(content) + 2* Number.parseFloat(padding) + 2*Number.parseFloat(border);
+    const result = Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
     return result;
 
 }
